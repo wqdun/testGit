59d05437ba6074d30654a70c1ccaa8220b79b1b9
dunwenqiang <dunwenqiang@navinfo.com>
2017-10-13 09:38:17 +0800

    局部变量的栈空间释放之后，再访问指向它的指针是错误的。

--- test.cc
+++ test.cc
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <map>
 #include <cmath>
+#include <vector>
+
 using namespace std;
 
+
+int *pi;
+
+typedef struct {
+    vector<vector<int>> vVInt;
+    int i;
+} vv_t;
+
+vv_t *pvv;
+
+
+int fun() {
+    vector<int> v = {100, 2, 3, 4, 7};
+
+    vv_t vv;
+    vv.vVInt.push_back(v);
+    pvv = &vv;
+}
+
+
 int main() {
-    double gpsTime = 368123.32;
-    double daySecond = fmod(gpsTime, 86400); // gpsTime % (24 * 3600);
-    cout << fixed << daySecond << endl;
+    fun();
+
+    cout << fixed << (pvv->vVInt)[0][1] << endl;
+
+    *pi = pvv->i;
+    cout << fixed << *pi << endl;
+
+    // free(pi);
+    // cout << fixed << *pi << endl;
 }
