89e928a0c1368941c2bd51cae4499110fb38fd8e
dunwenqiang <dunwenqiang@navinfo.com>
2017-10-16 13:45:53 +0800

    Unix时间到天秒的转换。

--- test.cc
+++ test.cc
@@ -7,70 +7,44 @@
 #include <vector>
 
 using namespace std;
-  typedef struct {
-    double pitch;
-    double roll;
-    double heading;
-    // geometry_msgs::Point currentWGS;
-    vector<double> vInt;
-  } imuInfo_t;
-imuInfo_t LookUpMap(const map<double, imuInfo_t> &map, const double x);
+double getDaySecond(const double rosTime, const double pktTime) {
+  int rosHour = (int)rosTime / 3600 % 24;
+  const int rosMinute = (int)rosTime / 60 % 60;
+  const int pktMinute = (int)pktTime / 60;
+  const int errMinute = rosMinute - pktMinute;
+  cout << rosHour << ":" << rosMinute << ":" << fmod(rosTime, 60) << endl;
+  cout << "00:" << pktMinute << endl;
+  cout << errMinute<< endl;
+
+  if(errMinute > 10) {
+    // cout << "errMinute > 10" << endl;
+    ++rosHour;
+  }
+  else
+  if(errMinute < -10) {
+    // cout << "errMinute < -10" << endl;
+    --rosHour;
+  }
+  // else {
+  //   // do nothing when errMinute in [-10, 10]
+  // }
+
+  // in case: -1 || 24
+  rosHour = (rosHour + 24) % 24;
+
+  return pktTime + 3600 * rosHour;
+}
 
 
 int main() {
-    // cout << fixed << 11345 % 60 << endl;
+    double dayS = getDaySecond(1508111941, 12);
 
-    map<double, imuInfo_t> temp;
-    temp[1.34] = {1, 2, 3};
-    imuInfo_t tempImu;
-    tempImu.vInt = {1, 2, 3};
-    tempImu = {0};
-    cout << "tempImu.heading" << tempImu.heading << endl;
-    cout << "tempImu.heading" << tempImu.vInt.size() << endl;
+    double dayHour = (int)dayS / 3600;
+    double dayMinute = (int)dayS / 60 % 60;
+    double daySecond = fmod(dayS, 60);
+    cout << dayHour << ":" << dayMinute << ":" << daySecond << endl;
+    cout << fixed << dayS << endl;
 
-    cout << temp.size() << endl;
-    cout << int(temp.empty()) << endl;
-    cout << LookUpMap(temp, 1).heading << endl;
 
 
 }
-
-imuInfo_t LookUpMap(const map<double, imuInfo_t> &map, const double x) {
-    // ROS_INFO_STREAM("map.size():" << map.size() << ":x:" << x);
-    const auto little_iter = map.cbegin();
-    const auto big_iter = map.crbegin();
-    cout << little_iter->first << endl;
-    if(x <= little_iter->first) {
-      // ROS_INFO_STREAM(x << " is too small.");
-      return little_iter->second;
-  }
-    if(x >= big_iter->first) {
-      // ROS_INFO_STREAM(x << " is too big.");
-      return big_iter->second;
-    }
-
-    const auto next = map.lower_bound(x);
-    // to float type, 2.0000000000000000000001 might be equal to 2
-    // in this case, --temp may fail
-    if(next == little_iter) {
-      // ROS_INFO_STREAM(x << " is the small endpoint.");
-      return little_iter->second;
-    }
-
-    auto temp = next;
-    const auto previous = (--temp);
-
-    // calculate correspond imuInfo
-    imuInfo_t tempImuInfo;
-    tempImuInfo.pitch = (next->second.pitch - previous->second.pitch) / (next->first - previous->first) * (x - previous->first) + previous->second.pitch;
-    tempImuInfo.roll = (next->second.roll - previous->second.roll) / (next->first - previous->first) * (x - previous->first) + previous->second.roll;
-    tempImuInfo.heading = (next->second.heading - previous->second.heading) / (next->first - previous->first) * (x - previous->first) + previous->second.heading;
-    // geometry_msgs::Point tempPoint;
-
-    // tempImuInfo.currentWGS.x = (next->second.currentWGS.x - previous->second.currentWGS.x) / (next->first - previous->first) * (x - previous->first) + previous->second.currentWGS.x;
-    // tempImuInfo.currentWGS.y = (next->second.currentWGS.y - previous->second.currentWGS.y) / (next->first - previous->first) * (x - previous->first) + previous->second.currentWGS.y;
-    // tempImuInfo.currentWGS.z = (next->second.currentWGS.z - previous->second.currentWGS.z) / (next->first - previous->first) * (x - previous->first) + previous->second.currentWGS.z;
-
-    return tempImuInfo;
-  }
-
