6f0ee89139235a3a356d31501fb4266f79773ced
dunwenqiang <dunwenqiang@navinfo.com>
2017-10-16 09:31:26 +0800

    使用map进行时间查表。

--- test.cc
+++ test.cc
@@ -7,35 +7,70 @@
 #include <vector>
 
 using namespace std;
-
-
-int *pi;
-
-typedef struct {
-    vector<vector<int>> vVInt;
-    int i;
-} vv_t;
-
-vv_t *pvv;
-
-
-int fun() {
-    vector<int> v = {100, 2, 3, 4, 7};
-
-    vv_t vv;
-    vv.vVInt.push_back(v);
-    pvv = &vv;
-}
+  typedef struct {
+    double pitch;
+    double roll;
+    double heading;
+    // geometry_msgs::Point currentWGS;
+    vector<double> vInt;
+  } imuInfo_t;
+imuInfo_t LookUpMap(const map<double, imuInfo_t> &map, const double x);
 
 
 int main() {
-    fun();
+    // cout << fixed << 11345 % 60 << endl;
 
-    cout << fixed << (pvv->vVInt)[0][1] << endl;
+    map<double, imuInfo_t> temp;
+    temp[1.34] = {1, 2, 3};
+    imuInfo_t tempImu;
+    tempImu.vInt = {1, 2, 3};
+    tempImu = {0};
+    cout << "tempImu.heading" << tempImu.heading << endl;
+    cout << "tempImu.heading" << tempImu.vInt.size() << endl;
 
-    *pi = pvv->i;
-    cout << fixed << *pi << endl;
+    cout << temp.size() << endl;
+    cout << int(temp.empty()) << endl;
+    cout << LookUpMap(temp, 1).heading << endl;
 
-    // free(pi);
-    // cout << fixed << *pi << endl;
+
 }
+
+imuInfo_t LookUpMap(const map<double, imuInfo_t> &map, const double x) {
+    // ROS_INFO_STREAM("map.size():" << map.size() << ":x:" << x);
+    const auto little_iter = map.cbegin();
+    const auto big_iter = map.crbegin();
+    cout << little_iter->first << endl;
+    if(x <= little_iter->first) {
+      // ROS_INFO_STREAM(x << " is too small.");
+      return little_iter->second;
+  }
+    if(x >= big_iter->first) {
+      // ROS_INFO_STREAM(x << " is too big.");
+      return big_iter->second;
+    }
+
+    const auto next = map.lower_bound(x);
+    // to float type, 2.0000000000000000000001 might be equal to 2
+    // in this case, --temp may fail
+    if(next == little_iter) {
+      // ROS_INFO_STREAM(x << " is the small endpoint.");
+      return little_iter->second;
+    }
+
+    auto temp = next;
+    const auto previous = (--temp);
+
+    // calculate correspond imuInfo
+    imuInfo_t tempImuInfo;
+    tempImuInfo.pitch = (next->second.pitch - previous->second.pitch) / (next->first - previous->first) * (x - previous->first) + previous->second.pitch;
+    tempImuInfo.roll = (next->second.roll - previous->second.roll) / (next->first - previous->first) * (x - previous->first) + previous->second.roll;
+    tempImuInfo.heading = (next->second.heading - previous->second.heading) / (next->first - previous->first) * (x - previous->first) + previous->second.heading;
+    // geometry_msgs::Point tempPoint;
+
+    // tempImuInfo.currentWGS.x = (next->second.currentWGS.x - previous->second.currentWGS.x) / (next->first - previous->first) * (x - previous->first) + previous->second.currentWGS.x;
+    // tempImuInfo.currentWGS.y = (next->second.currentWGS.y - previous->second.currentWGS.y) / (next->first - previous->first) * (x - previous->first) + previous->second.currentWGS.y;
+    // tempImuInfo.currentWGS.z = (next->second.currentWGS.z - previous->second.currentWGS.z) / (next->first - previous->first) * (x - previous->first) + previous->second.currentWGS.z;
+
+    return tempImuInfo;
+  }
+
