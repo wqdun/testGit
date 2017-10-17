320925742cb91d78fae7552106d0e3a63665485d
dunwenqiang <dunwenqiang@navinfo.com>
2017-10-17 09:01:49 +0800

    multimap的插入、遍历等，multimap仍然是有序的，如果关键字相同的话按照插入时的顺序。

--- test.cc
+++ test.cc
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <map>
 
-namespace ClassFoo {
-
 using namespace std;
 
 void PrintIntIntMap(std::map<int,int>& m) {
@@ -12,29 +10,47 @@ void PrintIntIntMap(std::map<int,int>& m) {
     std::cout << std::endl;
 }
 void MapEndExample1() {
-    std::map<int,int> foo1;
+    std::multimap<int,int> foo1;
 
-    foo1[3] = 96;
-    foo1[24] = 14;
-    foo1[35] = 37;
+    // foo1[3] = 96;
+    // foo1[3] = 100;
+    // foo1[24] = 14;
+    // foo1[35] = 37;
+    foo1.insert(std::make_pair(22, 6));
+    foo1.insert(std::make_pair(3, 1000));
+    foo1.insert(std::make_pair(3, 96));
+    foo1.insert(std::make_pair(3, 333));
+    foo1.insert(std::make_pair(4, 96));
+    cout << foo1.size() << endl;
+    // cout << foo1[3] << endl;
+    for(const auto &ele: foo1) {
+      cout << ele.first << ele.second << endl;
+    }
+  }
+
+    // cout << foo1.begin()->first << endl;
+    // // cout << foo1.end()->first << endl;
+    // cout << (int)(foo1.rbegin() == foo1.rend()) << endl;
+    // cout << foo1.rbegin()->first << endl;
+    // // cout << foo1.rend()->first << endl;
+    // auto beginIter = foo1.lower_bound(0);
+    // cout << (int)(beginIter == foo1.cend()) << endl;
+    // cout << (int)(foo1.upper_bound(35) == foo1.cend()) << endl;
+
+    // cout << foo1.upper_bound(.9)->second << endl;
+
+    // auto tmp = foo1.end();
+    // --tmp;
+    // // foo1.erase(foo1.begin(), tmp);
+    // foo1.erase(foo1.begin(), foo1.begin());
     // cout << foo1.size() << endl;
-    cout << foo1.begin()->first << endl;
-    // cout << foo1.end()->first << endl;
-    cout << (int)(foo1.rbegin() == foo1.rend()) << endl;
-    cout << foo1.rbegin()->first << endl;
-    // cout << foo1.rend()->first << endl;
-    auto beginIter = foo1.lower_bound(0);
-    cout << (int)(beginIter == foo1.cend()) << endl;
-    cout << (int)(foo1.upper_bound(35) == foo1.cend()) << endl;
+    // cout << foo1.begin()->first << endl;
 
-    cout << foo1.upper_bound(.9)->second << endl;
     // PrintIntIntMap(foo1);
-}
+// }
 
-
-}
 int main ()
 {
-    ClassFoo::MapEndExample1();
+    MapEndExample1();
     return 0;
 }
