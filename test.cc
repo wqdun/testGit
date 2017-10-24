64c8976dd174eec32a65dd14e3d026a44c3d70c2
dunwenqiang <dunwenqiang@navinfo.com>
2017-10-24 10:24:30 +0800

    测试二进制文件读写。

--- test.cc
+++ test.cc
@@ -1,40 +1,109 @@
-#include <fstream>
 #include <iostream>
-#include <stdlib.h>
+#include <fstream>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+#include <errno.h>
+
 using namespace std;
 
-struct student
-{
-   char name[20];
-   int num;
-   int age;
-   char sex;
-};
-int main( )
-{
-   student stud[3]={"Li",1001,18,'f',"Fun",1002,19,'m',"Wang",1004,17,'f'};
-   fstream iofile("stud.dat",ios::in|ios::out|ios::binary);
-   if(!iofile)
-   {
-      cout<<"open error!"<<endl;
-      // abort( );//退出程序
-      exit(1);
-   }
-    // for(int i=0;i<3;i++)
-    //     iofile.write((char*)&stud[i],sizeof(stud[i]));
-    iofile.write((char*)&stud[0],sizeof(stud));
+typedef struct point {
+    double x;
+    double y;
+    double z;
+    double intensity;
+    double daySecond;
+} point_t;
 
-    // student stud[3];
-    iofile.read((char*)&stud[0],sizeof(stud));
 
-    iofile.close( );
-       for(int i=0;i<3;i++)
-   {
-      cout<<"NO."<<i+1<<endl;
-      cout<<"name:"<<stud[i].name<<endl;
-      cout<<"num:"<<stud[i].num<<endl;;
-      cout<<"age:"<<stud[i].age<<endl;
-      cout<<"sex:"<<stud[i].sex<<endl<<endl;
-   }
-    return 0;
+void WriteBINFile_C()
+{
+    FILE* fp = fopen("c.bin","wb");
+    if(!fp) {
+        cout << "open failed with errno:" << errno << endl;
+    }
+    
+    point_t p1 = {1, 2, 3, 4, 5};
+    point_t p2 = {2, 3, 4, 5, 6};
+    point_t ps[100] = {p1, p2};
+    
+    string str = "-7.231225:1.987737:-2.009471:13.000000:22144.888645\n";
+    const char *cstr = str.c_str();
+    int len = strlen(cstr);
+    cout << len << endl;
+
+    fwrite(cstr, 1, len, fp);
+    fwrite(cstr, 1, len, fp);
+
+    fclose(fp);
+}
+
+void ReadBINFile_C()
+{
+    // FILE* fp = fopen("/home/dun/.ros/19_01_53.lidar","rb");
+    FILE* fp = fopen("c.bin","rb");
+    char str[5500000];
+
+    // for(int i = 0; i < 1000; i++)
+    // {
+    //     for(int j = 0; j < 1000; j++)
+    //     {
+            fread(str, 2, 2, fp);
+    //     }
+    // }
+    // str[5500000] = '\0';
+    cout << strlen(str) << endl;
+    cout << str[2] << (int)str[5] << endl;
+    fclose(fp);
+}
+
+
+// bool DealFile(string fileName) //随便写个函数说明
+// {
+//     FILE *file;
+// unsigned long int fileSize ,pos;
+// int readLen ;
+
+// //MAX_BUFFER_LEN 在头文件里定义，这里能够保证数据不丢失，也不至于内存逸出
+// char *buffer = new char[MAX_BUFFER_LEN];
+// file = fopen(fileName.c_str(),"r+b");
+// if(file == NULL) return false;
+// fseek(file,0,2);
+// fileSize = ftell(file); //取得文件的大小
+// fseek(file,0,0);
+// do{
+// readLen = fread(buffer,sizeof(char),MAX_BUFFER_LEN,file);
+// if(readLen > 0)
+// {
+// pos += readLen;
+// //对读取的文件做处理
+// }
+// }while(pos < fileSize); //循环读取文件
+// delete[] buffer;
+// fclose(file); //释放资源
+// return true;
+
+// }
+
+
+int main()
+{
+    unsigned long int start, end;
+
+    sleep(5);
+    cout << "1000 ms" << endl;
+
+    // start = GetTickCount();
+    // WriteBINFile_C();
+    // // end = GetTickCount();
+    // printf("C语言写二进制文件操作运行时间为：%ld ms\n", end - start);
+    // ReadBINFile_C();
+    // DealFile();
+    // size_t tmp = 160000;
+    // cout << endl;
+    // cout << (tmp >> 8) << endl;
+    // cout << tmp / 256 << endl;
+
 }
