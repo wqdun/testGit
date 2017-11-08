3577f48781bd5455b2ac06d988da173f4ca2197b
dunwenqiang <dunwenqiang@navinfo.com>
2017-11-08 16:12:03 +0800

    使用getline实现分割字符串的功能。

--- test.cc
+++ test.cc
@@ -2,59 +2,27 @@
 using std::cout; using std::endl;
 #include <string>
 using std::string;
+#include <sstream>
+using std::istringstream;
 
-template <typename T>
-int WithStatic(T &v1) {
-    static int i = 0;
-    ++i;
-    cout << v1 << endl;
-    cout << "i = " << i << endl;
+int main()
+{
+    // 问候用户
+    // std::string name;
+    // std::cout << "What is your name? ";
+    // std::getline(std::cin, name);
+    // std::cout << "Hello " << name << ", nice to meet you.\n";
+
+    // 逐行读文件
+    std::istringstream input;
+    input.str("1\"2\"3\"4\"5\"6\"7\"");
+    int sum = 0;
+    std::string line;
+    while(std::getline(input, line, '\"')) {
+        cout << line;
+    }
+    for ( ; std::getline(input, line); ) {
+        sum += std::stoi(line);
+    }
+    std::cout << "\nThe sum is: " << sum << "\n";
 }
-
-int func1();
-int func2();
-
-int main() {
-    double d = 3.14, d2 = 3.1415;
-    float f = 3.1;
-    int i = 3;
-    string str = "Hello";
-
-    WithStatic(d);
-    WithStatic(f);
-    WithStatic(i);
-    WithStatic(str);
-    WithStatic(d2);
-
-    func1();
-    func2();
-}
-
-int func1() {
-    static int i = 0;
-    ++i;
-    ++i;
-    cout << i << "\n";
-}
-
-int func2() {
-    static int i = 0;
-    ++i;
-    cout << i << "\n";
-}
-
-
-// The output is:
-// 3.14
-// i = 1
-// 3.1
-// i = 1
-// 3
-// i = 1
-// Hello
-// i = 1
-// 3.1415
-// i = 2
-// 2
-// 1
-
