178a4ba210df3daaae61a4b1261c153a5b4b3bc5
dunwenqiang <dunwenqiang@navinfo.com>
2017-11-18 11:35:29 +0800

    流sstream转换成double；boost::split的实现。

--- test.cc
+++ test.cc
@@ -4,6 +4,9 @@
 #include <bitset>
 #include <string>
 #include <algorithm>
+#include <vector>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 
 using namespace std;
 
@@ -21,85 +24,57 @@ string toBinaryString(int inNum) {
     return binaryStringWith0Ahead.substr(binaryStringWith0Ahead.find("1"));
 }
 
+void func(void *p) {
+    ++(*((size_t *)p));
+    // ++(size_t)(*p);
+
+}
+
+
 int main() {
-    int a = 3;
-    cout << pow(2, 3) << endl;
-
-    const int bit = sizeof(a) << 3;
-    cout << "bit: " << bit << endl;
-    bitset<bit> bst(15);
-    std::ostringstream oss;
-    oss << bst;
-    string str2(oss.str());
-    cout << str2 << endl;
-    cout << str2.length() << endl;
-
-
-
-    for(int i = bit - 1; i >= 0; --i) {
-        cout << bst[i] << endl;
-        if(1 == bst[i]) {
-            cout << i + 1 << "\n";
-            break;
-        }
+    vector<int> mTileList = {20596447, 20596446};
+    string sql("");
+    for(auto tile: mTileList) {
+        sql += " or MESH = " + std::to_string(tile);
     }
 
+    sql = "SELECT GEOM FROM ROAD WHERE " + sql.substr(4);
+    cout << sql << "\n";
 
-    // cout << binary << bit << endl;
-    int bb = 12456;
-    for(int i = 0; i < 32; ++i) {
-        if(!(bb >> i)) {
-            cout << i << endl;
+    string str2parse = "LINESTRING (116.35233 39.9779)";
+    std::istringstream input("(116.35233 39.9779");
+    string line;
+    double lat;
+    input >> lat;
+    input >> lat;
+    // input >> lat;
+    // input >> lat;
+    // input >> lat;
+    cout << "lat:" << lat << endl;
+    // getline(input, line, ',');
+    // line = "(116.35233";
+    // cout << stod(line) << endl;
+    // // stoi(line);
+    // cout << line << endl;
+    // str2parse = "LINESTRING";
+    vector<string> parsed_data;
+    boost::split(parsed_data, str2parse, boost::is_any_of( ",)(" ), boost::token_compress_on);
+    cout << parsed_data.size() << endl;
+    // for(int i = 1; i < parsed_data.size() - 1; ++i) {
+    //     cout << parsed_data[i] << endl;
+    //     std::istringstream iss(parsed_data[i]);
+    //     double lat;
+    //     double lon;
+    //     iss >> lat >> lon;
+    //     cout << lon << " : " << lat << endl;
+    //     cout << fixed << lon << " : " << lat << endl;
+    // }
 
-            break;
-        }
-    }
+    // size_t i = 0;
+    // for(int ii = 0; ii < 10; ++ii)
+    //     func(&i);
+    // cout << i << endl;
 
-    cout << getDimensionCode(11, 2, 3) << endl;
-
-    string str="00000112003";
-    cout << str.substr(2)<<endl;
-    cout << str.substr(str.find("1"))<<endl;
-    string binaryStr("");
-    cout << (binaryStr = toBinaryString(17)) << endl;
-    // binaryStr = "0" + binaryStr;
-    binaryStr += "0";
-    cout << binaryStr << endl;
-
-
-    string str_dec = "2048,hello world";
-    string str_hex = "403";
-    string str_bin = "-10010110001";
-    string str_auto = "0x7f";
-
-    size_t sz;   // alias of size_t
-    int i_dec = std::stoi(str_dec, &sz);
-    int i_hex = std::stoi(str_hex, nullptr, 16);
-    int i_bin = std::stoi(str_bin, nullptr, 2);
-    int i_auto = std::stoi(str_auto, nullptr, 0);
-
-    std::cout << str_dec << ": " << i_dec << " and [" << str_dec.substr(sz) << "]\n";
-    std::cout << str_hex << ": " << i_hex << '\n';
-    std::cout << str_bin << ": " << i_bin << '\n';
-    std::cout << str_auto << ": " << i_auto << '\n';
-
-    string s = "010101111122";
-    reverse(s.begin(), s.end());
-    cout << s << endl;
-
-    string binaryStr2("01100100011011100100011011"), binaryXStr(""), binaryYStr("");
-    cout << binaryStr2.substr(2, 0 + 5) << endl;
-    for (int i = 0; i < binaryStr2.length(); i = i + 2) {
-        binaryYStr.append(binaryStr2.substr(i, 1));
-        cout << i << " : " << binaryYStr << endl;
-
-        if (binaryStr2.length() > i + 1) {
-            binaryXStr.append(binaryStr2.substr(i + 1, 1));
-        }
-    }
-
-    cout << binaryXStr << endl;
-    cout << binaryYStr << endl;
 
     return 0;
 }
